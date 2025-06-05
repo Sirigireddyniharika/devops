a947b6344ce522f50942940e158760d72c57d0c9
Sirigireddyniharika <niharikasirigireddy9@gmail.com>
2025-06-05 23:36:28 +0530

    "bala na "

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return "Hello,Bangaru konda, Bujji Kanna, Chinnulu, Nanilu,Bannulu! i am your Dharya you are my Kannaya Bangaram"
+    return "Hello,Bangaru konda, Bujji Kanna, Chinnulu, Nanilu,Bannulu! i am your Dharya you are my Kannaya Bangaram my husband"
 
 if __name__ == "__main__":
     import uvicorn
